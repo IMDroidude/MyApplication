9db6cf30cbdf0c727bd5466bb683e7345f27d905
mallaudin <mallaudinqazi@gmail.com>
2019-12-03 15:53:18 +0500

    - Terms and condition updated

--- app/src/main/java/quiz/mania/trivia/mcq/question/MainActivity.kt
+++ app/src/main/java/quiz/mania/trivia/mcq/question/MainActivity.kt
@@ -1,12 +1,24 @@
 package quiz.mania.trivia.mcq.question
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import androidx.annotation.StringRes
 import quiz.mania.trivia.mcq.question.common.AlertStore
 import quiz.mania.trivia.mcq.question.common.BaseActivity
 import quiz.mania.trivia.mcq.question.databinding.ActivityMainBinding
 import quiz.mania.trivia.mcq.question.home.MainViewModel
 import quiz.mania.trivia.mcq.question.testpackage.TestClass
 import javax.inject.Inject
+import kotlin.reflect.KClass
 
 ///@ContributesActivity
 class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
@@ -28,6 +40,8 @@ class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
         ///viewModel = getViewModel()
         mViewModel.showDialog()
 
+
+        spanTermsAndConditions()
         //viewModel = ViewModelProviders.of(this,MainModelFactory(AlertStore(this@MainActivity)))[MainViewModel::class.java]
 
         //viewModel.showDialog()
@@ -37,4 +51,87 @@ class MainActivity : BaseActivity<ActivityMainBinding,MainViewModel>() {
 
     override val layoutId: Int
         get() = R.layout.activity_main
+
+    private fun spanTermsAndConditions() {
+
+        val termsAndConditionSpan = clickableSpan {
+            openBrowser("https://transferhome.org/app/terms")
+        }
+
+        val privacyPolicySpan = clickableSpan {
+            openBrowser("https://transferhome.org/app/privacy")
+        }
+
+        val html = fromHtml(R.string.terms_and_conditions)
+        val termsAndConditionSpannable = SpannableString(html)
+
+        val termsAndConditionString = html.subSequence(0, html.length)
+        val termsText = getString(R.string.terms_and_cond)
+
+        val termsStartIndex = termsAndConditionString.indexOf(termsText)
+        val termsEndIndex = termsStartIndex + termsText.length
+
+
+        termsAndConditionSpannable.setSpan(
+            termsAndConditionSpan, termsStartIndex, termsEndIndex,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val policyString = getString(R.string.privacy_policy)
+        val policyStartIndex = termsAndConditionString.indexOf(policyString)
+        val policyEndIndex = policyStartIndex + policyString.length
+
+        termsAndConditionSpannable.setSpan(
+            privacyPolicySpan, policyStartIndex, policyEndIndex,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        mBinding.terms.text = termsAndConditionSpannable
+        mBinding.terms.movementMethod = LinkMovementMethod.getInstance()
+
+    }
+
+    fun clickableSpan(onClick: (widget: View) -> Unit): ClickableSpan {
+        return object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                onClick.invoke(widget)
+            }
+        }
+    }
+
+    /*
+
+        Activity Extension are writtern below
+     */
+    fun Activity.openBrowser(url: String) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        if (browserIntent.resolveActivity(packageManager) != null) {
+            openActivity(browserIntent)
+        }
+    }
+
+    fun Activity.fromHtml(@StringRes stringRes: Int): Spanned {
+        val textToSpan = getString(stringRes)
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Html.fromHtml(textToSpan, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(textToSpan)
+        }
+    }
+
+    fun Activity.openActivityForResult(intent: Intent, reqCode: Int) {
+        startActivityForResult(intent, reqCode)
+    }
+
+    fun Activity.openActivity(nextActivity: KClass<*>) {
+        val intent = Intent(this, nextActivity.java)
+        openActivity(intent)
+    }
+
+    fun Activity.openActivity(intent: Intent) {
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
 }
